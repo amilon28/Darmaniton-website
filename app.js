4864508e5c2b901f708c44c450ee0c35844945c6
Amin Gharibshah <amingharibshah@gmail.com>
2021-07-19 20:45:46 +0430

    Finish responsive for big-phone, tab-port

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const closeMenu = document.querySelector(".close-menu");
 
 hamburgerBtn.addEventListener("click", function () {
   mobileMenu.style.transform = "translateX(0)";
-  hamburgerBtn.style.display = "none";
 });
 
 closeMenu.addEventListener("click", function () {
   mobileMenu.style.transform = "translateX(105%)";
-  hamburgerBtn.style.display = "block";
 });
